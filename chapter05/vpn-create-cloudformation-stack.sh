a5d95b2f4a6bcb5ea8836c6a61517e76a41a890b
Michael Wittig <post@michaelwittig.info>
2017-08-15 10:33:59 +0200

    fixed path

--- chapter05/vpn-create-cloudformation-stack.sh
+++ chapter05/vpn-create-cloudformation-stack.sh
@@ -5,7 +5,7 @@ SubnetId=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$VpcId --query 
 SharedSecret=$(openssl rand -base64 30)
 Password=$(openssl rand -base64 30)
 
-aws cloudformation create-stack --stack-name vpn --template-url https://s3.amazonaws.com/awsinaction-code2/chapter05/vpn-cloudformation.json --parameters ParameterKey=KeyName,ParameterValue=mykey ParameterKey=VPC,ParameterValue=$VpcId ParameterKey=Subnet,ParameterValue=$SubnetId "ParameterKey=IPSecSharedSecret,ParameterValue=$SharedSecret" ParameterKey=VPNUser,ParameterValue=vpn "ParameterKey=VPNPassword,ParameterValue=$Password"
+aws cloudformation create-stack --stack-name vpn --template-url https://s3.amazonaws.com/awsinaction-code2/chapter05/vpn-cloudformation.yaml --parameters ParameterKey=KeyName,ParameterValue=mykey ParameterKey=VPC,ParameterValue=$VpcId ParameterKey=Subnet,ParameterValue=$SubnetId "ParameterKey=IPSecSharedSecret,ParameterValue=$SharedSecret" ParameterKey=VPNUser,ParameterValue=vpn "ParameterKey=VPNPassword,ParameterValue=$Password"
 
 aws cloudformation wait stack-create-complete --stack-name vpn
 
