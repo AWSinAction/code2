fce62370001344345fb5f2eb1adaadf059680e18
Andreas Wittig <andreas@widdix.de>
2017-12-01 20:31:34 +0100

    final versio of example for chapter 7

--- chapter07/lambda_function.py
+++ chapter07/lambda_function.py
@@ -1,19 +1,15 @@
 
 import boto3
 ec2 = boto3.client('ec2')
+
 def lambda_handler(event, context):
 
-  eventName = event['detail']['eventName']
-
-  if eventName == 'RunInstances':
-    userName = event['detail']['userIdentity']['arn'].split('/')[1]
-    instanceId = event['detail']['responseElements']['instancesSet']['items'][0]['instanceId']    
-    print("Adding owner tag " + userName + " to instance " + instanceId + ".")
-    try:
-      ec2.create_tags(Resources=[instanceId,],Tags=[{'Key': 'Owner', 'Value': userName},])
-      return
-    except Exception as e:
-      print(e)
-      raise e
-  else:
+  userName = event['detail']['userIdentity']['arn'].split('/')[1]
+  instanceId = event['detail']['responseElements']['instancesSet']['items'][0]['instanceId']    
+  print("Adding owner tag " + userName + " to instance " + instanceId + ".")
+  try:
+    ec2.create_tags(Resources=[instanceId,],Tags=[{'Key': 'Owner', 'Value': userName},])
     return
+  except Exception as e:
+    print(e)
+    raise e
