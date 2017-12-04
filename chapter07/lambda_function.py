092363f8a7de652a22c9cc65229898fa24a90575
Andreas Wittig <andreas@widdix.de>
2017-12-04 17:43:57 +0100

    removed useless error handling

--- chapter07/lambda_function.py
+++ chapter07/lambda_function.py
@@ -7,9 +7,5 @@ def lambda_handler(event, context):
   userName = event['detail']['userIdentity']['arn'].split('/')[1]
   instanceId = event['detail']['responseElements']['instancesSet']['items'][0]['instanceId']    
   print("Adding owner tag " + userName + " to instance " + instanceId + ".")
-  try:
-    ec2.create_tags(Resources=[instanceId,],Tags=[{'Key': 'Owner', 'Value': userName},])
-    return
-  except Exception as e:
-    print(e)
-    raise e
+  ec2.create_tags(Resources=[instanceId,],Tags=[{'Key': 'Owner', 'Value': userName},])
+  return
