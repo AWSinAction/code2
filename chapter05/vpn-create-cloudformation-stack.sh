cc1119c5a8e31676cc1e6c71f171c3520f0d8282
Michael Wittig <post@michaelwittig.info>
2017-12-19 12:05:31 +0100

    quote all the queries and filters

--- chapter05/vpn-create-cloudformation-stack.sh
+++ chapter05/vpn-create-cloudformation-stack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 VpcId=$(aws ec2 describe-vpcs --filter "Name=isDefault, Values=true" --query "Vpcs[0].VpcId" --output text)
-SubnetId=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VpcId" --query Subnets[0].SubnetId --output text)
+SubnetId=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VpcId" --query "Subnets[0].SubnetId" --output text)
 SharedSecret=$(openssl rand -base64 30)
 Password=$(openssl rand -base64 30)
 
@@ -9,4 +9,4 @@ aws cloudformation create-stack --stack-name vpn --template-url https://s3.amazo
 
 aws cloudformation wait stack-create-complete --stack-name vpn
 
-aws cloudformation describe-stacks --stack-name vpn --query Stacks[0].Outputs
+aws cloudformation describe-stacks --stack-name vpn --query "Stacks[0].Outputs"
