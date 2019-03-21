b374923cb0f6f6d4dad8fb40783e622f7fdced63
Michael Wittig <michael@widdix.de>
2019-03-21 09:39:59 +0100

    fix example: no CloudWatch Events are triggered if CloudTrail is not active

--- chapter07/lambda_function.py
+++ chapter07/lambda_function.py
@@ -1,10 +1,12 @@
-
 import boto3
 ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
-
-  userName = event['detail']['userIdentity']['arn'].split('/')[1]
+  print(event)
+  if "/" in event['detail']['userIdentity']['arn']:
+  	userName = event['detail']['userIdentity']['arn'].split('/')[1]
+  else:
+  	userName = event['detail']['userIdentity']['arn']
   instanceId = event['detail']['responseElements']['instancesSet']['items'][0]['instanceId']    
   print("Adding owner tag " + userName + " to instance " + instanceId + ".")
   ec2.create_tags(Resources=[instanceId,],Tags=[{'Key': 'Owner', 'Value': userName},])
